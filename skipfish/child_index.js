0350015afdc6be0b7af14db953c641bb8d942dfc
Manan Sethia <manans5040@gmail.com>
2026-02-18 21:01:42 +0530

    pages

--- skipfish/child_index.js
+++
@@ -1,3 +0,0 @@
-var child = [
-  { 'dupe': false, 'type': 2, 'name': 'https://manansethia.com/', 'dir': 'c0', 'linked': 2, 'url': 'https://manansethia.com/', 'fetched': false, 'error': 'Connection error', 'missing': false, 'csens': false, 'child_cnt': 0, 'issue_cnt': [ 0, 1, 0, 0, 0 ], 'sig': 0x6b599831 }
-];
--- skipfish/summary.js
+++
@@ -1,4 +0,0 @@
-var sf_version = '2.10b';
-var scan_date  = 'Sat Feb  7 11:37:11 2026';
-var scan_seed  = '0xd68176eb';
-var scan_ms    = 3547;
